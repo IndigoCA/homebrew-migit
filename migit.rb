59067e7e12e27359fb7c4b71bbd3f4925ad19d52
Christopher Shum <shum.chris@gmail.com>
2014-04-13 16:35:24 -0400

    updated formula for minor changes

--- migit.rb
+++ migit.rb
@@ -4,7 +4,7 @@ class Migit < Formula
   homepage "https://bitbucket.org/chaty/crazygit/src"
   url "http://www.nassaralec.com/migit.tar.gz"
   version "1.0"
-  sha1 "9551a74f9714ca05fe62e429939dc5402e487489"
+  sha1 "f8a0b94128eea62380a209ad0a70c9e8a18118e0"
 
   def install
     system "./configure", "--prefix=#{prefix}"
